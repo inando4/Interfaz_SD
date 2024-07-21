da506fb7e9d9138864e096cd5a85634c3331768e
inando4 <torresluisperez@hotmail.com>
2024-07-21 01:20:04 -0500

    Avance, cambio de logica con keycloak

--- src/App.js
+++ src/App.js
@@ -3,59 +3,16 @@ import VotingList from './components/VotingList';
 import CandidateTemplates from './components/CandidateTemplates';
 import AdminDashboard from './components/AdminDashboard';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Keycloak from 'keycloak-js';
-
-
-const keycloakOptions = {
-  url: "http://localhost:8080/",
-  realm: "reino-prueba",
-  clientId: "react-app-client",
-};
-
-const kc = new Keycloak(keycloakOptions);
-
-kc.init({
-  onLoad: 'login-required', // Supported values: 'check-sso' , 'login-required'
-  checkLoginIframe: true,
-}).then((auth) => {
-  if (!auth) {
-    window.location.reload();
-  } else {
-    /* Remove below logs if you are using this on production */
-    console.info("Authenticated");
-    console.log('auth', auth)
-    console.log('Keycloak', kc)
-    console.log('Access Token', kc.token)
-    console.log(kc)
-    const nombreUsuario = kc.tokenParsed.name;
-    console.log('Nombre Usuario:', nombreUsuario)
-    kc.onTokenExpired = () => {
-      console.log('token expired')
-    }
-  }
-}, () => {
-  /* Notify the user if necessary */
-  console.error("Authentication Failed");
-});
-
-const handleLogout = () => {
-  if (kc) {
-    kc.logout();
-  }
-}
-
-const getNombreUsuario = () => {
-  if (kc && kc.tokenParsed) {
-    return kc.tokenParsed.name;
-  }
-  return ''; // Retorna una cadena vacía o un valor predeterminado si kc o kc.tokenParsed no están definidos
-};
+import React from 'react';
+import keycloak from './keycloak'; // Importa la instancia de Keycloak
 
 function App() {
-  const nombreUsuario = getNombreUsuario(); // Paso 1 y 2
+  const handleLogout = () => {
+    keycloak.logout();
+  };
   return (
     <Router>
-      <div>
+        <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
           <div className="container-fluid">
             <a className="navbar-brand" href="/">Sistema Electoral</a>
@@ -73,8 +30,6 @@ function App() {
             </div>
           </div>
         </nav>
-        <div>
-          <h2>Bienvenido: {nombreUsuario}</h2>
         </div>
         <div className="App">
           <Routes>
@@ -84,7 +39,6 @@ function App() {
             {/* Aquí puedes agregar más rutas para otras páginas */}
           </Routes>
         </div>
-      </div>
     </Router>
   );
 }
