8e809e928688e4302c427f1e3a7f876e0779412a
inando4 <torresluisperez@hotmail.com>
2024-07-22 00:17:48 -0500

    Mejora de lista de elecciones.

--- src/components/VotingList.js
+++ src/components/VotingList.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import { ListGroup, Container, Row, Col } from 'react-bootstrap';
-import './VotingList.css'; // Opcional: para estilos
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import './VotingList.css'; // Asegúrate de tener un archivo CSS para estilos personalizados
 
 const VotingList = () => {
   const elections = [
     { id: 1, name: 'Elecciones Presidenciales' },
     { id: 2, name: 'Elecciones para Alcaldía' },
     { id: 3, name: 'Elecciones de Universidad' },
+    { id: 4, name: 'Elecciones de Municipio' },
+    { id: 5, name: 'Elecciones de Region Arequipa' },
+    { id: 6, name: 'Elecciones de la UNSA' },
     // Agrega más elecciones según sea necesario
   ];
 
   return (
-    <Container className="voting-list">
+    <Container className="voting-list mt-4">
+      <h1 className="text-center mb-4">Lista de Votaciones</h1>
       <Row>
-        <Col>
-          <h1>Lista de Votaciones</h1>
-          <ListGroup>
-            {elections.map(election => (
-              <ListGroup.Item key={election.id} action href={`/election/${election.id}`}>
-                {election.name}
-              </ListGroup.Item>
-            ))}
-          </ListGroup>
-        </Col>
+        {elections.map((election, index) => (
+          <Col key={election.id} md={6} className="mb-4 d-flex justify-content-center">
+            <Card className="w-100"> {/* Ajusta el ancho de la tarjeta aquí */}
+              <Card.Body className="d-flex justify-content-between align-items-center">
+                <Card.Title>{election.name}</Card.Title>
+                <Button variant="primary" href={`/election/${election.id}`}>Ver Detalles</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
       </Row>
     </Container>
   );
